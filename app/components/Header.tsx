4a15c349dc978b37fdd77ad6de90ef4b14070a97
thiagocontelli <thiagocontellid@gmail.com>
2023-06-20 17:53:44 -0300

    Add user menu with sign out option to the header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,8 +1,14 @@
 'use client'
 
+import { Menu, Transition } from "@headlessui/react";
 import { signOut, useSession } from "next-auth/react";
+import Image from 'next/image';
+import { Fragment } from "react";
 import { GoogleButton } from "./GoogleButton";
-import { SignOut } from "@phosphor-icons/react";
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
+}
 
 export function Header() {
   const { data: session } = useSession()
@@ -12,17 +18,45 @@ export function Header() {
       <span className="font-bold text-2xl">Blog.</span>
 
       {session ? (
-        <button
-          onClick={() => signOut()}
-          className="flex gap-4 items-center transition-all duration-200 hover:bg-red-500 hover:text-white py-2 px-4 rounded text-red-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-        >
-          Sign out
-          <SignOut size={20} />
-        </button>
+        <Menu as="div" className="relative ml-3">
+          <div>
+            <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+              <span className="sr-only">Open user menu</span>
+              <Image
+                className="rounded-full"
+                src={session.user?.image || ''}
+                alt="User menu"
+                width={40}
+                height={40}
+              />
+            </Menu.Button>
+          </div>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+              <Menu.Item>
+                {({ active }) => (
+                  <span
+                    className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700 cursor-pointer')}
+                    onClick={() => signOut()}
+                  >
+                    Sign out
+                  </span>
+                )}
+              </Menu.Item>
+            </Menu.Items>
+          </Transition>
+        </Menu>
       ) : (
         <GoogleButton />
       )}
-
     </div>
   )
 }
