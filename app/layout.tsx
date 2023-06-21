676c4abf352ff4bfced2105895167cb1ac86e919
thiagocontelli <thiagocontellid@gmail.com>
2023-06-21 20:52:32 -0300

    Move the Header to the default layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Header } from './components'
 import ProvidersWrapper from './components/ProvidersWrapper'
 import './globals.css'
 import { Inter } from 'next/font/google'
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ProvidersWrapper>
+          <Header />
           {children}
         </ProvidersWrapper>
       </body>
