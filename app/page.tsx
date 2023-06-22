0497efa1e4cae80219081090cee6ff1d0142de69
thiagocontelli <thiagocontellid@gmail.com>
2023-06-22 20:11:16 -0300

    Create a grid of posts on the home page and make it responsive

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,55 @@
+import { Post } from '@/models/Post'
+import { api } from '@/server/api/client'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default async function Home() {
-  const response = await fetch('http://localhost:3001/api/posts', { method: 'GET', cache: 'no-store' })
-  const posts = await response.json()
+  const response = await api.get<Post[]>('posts', { cache: 'no-store' })
+  const posts = response.map(item => new Post(item.id, item.title, item.content, new Date(item.createdAt), item.user))
 
   const dateFormatter = new Intl.DateTimeFormat('en-US', { dateStyle: 'long' })
 
   return (
-    <div className="flex justify-between flex-wrap gap-8 p-8">
+    <div className="grid lg:grid-cols-4 md:grid-cols-2 gap-8 p-8">
       {posts.map((post: any) =>
         <article
           key={post.id}
-          className="flex p-4 w-[350px] flex-col items-start justify-between border-2 rounded hover:cursor-pointer hover:bg-gray-50 active:scale-95 transition-transform"
+          className=' hover:bg-gray-50 active:scale-95 transition-transform hover:cursor-pointer p-4 border-2 rounded'
         >
-          <div className="flex items-center gap-x-4 text-xs">
-            <time dateTime={post.createdAt} className="text-gray-500">
-              {dateFormatter.format(new Date(post.createdAt))}
-            </time>
-          </div>
-          <div className="group relative">
-            <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900">
-              <a>
-                <span className="absolute inset-0" />
-                {post.title}
-              </a>
-            </h3>
-            <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{post.content}</p>
-          </div>
-          <div className="relative mt-8 flex items-center gap-x-4">
-            <Image src={post.user.image} alt="User profile picture" width={40} height={40} className="rounded-full" />
-            <div className="text-sm leading-6">
-              <p className="font-semibold text-gray-900">
-                <span className="absolute inset-0" />
-                {post.user.name}
-              </p>
+          <Link
+            href={`posts/${post.id}`}
+            className='flex flex-col items-start justify-between h-full'
+          >
+            <div className="flex items-center gap-x-4 text-xs">
+              <time dateTime={post.createdAt} className="text-gray-500">
+                {dateFormatter.format(new Date(post.createdAt))}
+              </time>
             </div>
-          </div>
+            <div className="group relative">
+              <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900">
+                <p>
+                  <span className="absolute inset-0" />
+                  {post.title}
+                </p>
+              </h3>
+              <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{post.content}</p>
+            </div>
+            <div className="relative mt-8 flex items-center gap-x-4">
+              <Image
+                src={post.user.image}
+                alt="User profile picture"
+                width={40}
+                height={40}
+                className="rounded-full"
+              />
+              <div className="text-sm leading-6">
+                <p className="font-semibold text-gray-900">
+                  <span className="absolute inset-0" />
+                  {post.user.name}
+                </p>
+              </div>
+            </div>
+          </Link>
         </article>
       )}
     </div>
