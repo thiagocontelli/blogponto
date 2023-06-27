e31afc5edca50608ff5ab0e7272cfb10d31260d8
Thiago Contelli <thiagocontellid@gmail.com>
2023-06-27 14:17:13 -0300

    Delete Post.ts

--- models/Post.ts
+++
@@ -1,9 +0,0 @@
-export class Post {
-  constructor(
-    readonly id: string,
-    readonly title: string,
-    readonly content: string,
-    readonly description: string,
-    readonly createdAt: Date,
-  ) {}
-}
